5acdbf149b77b5cef7db0c9a0935d9a3d81fef97
unknown <nnewman316@gmail.com>
2019-01-25 17:48:55 -0800

    added line to copy img dir on windows

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,7 @@
-pyinstaller --onefile --name="Vision Tuner" --noconfirm ^
-    --distpath=output/dist --workpath=output/build --specpath=output/spec ^
-    --hidden-import="numpy.core._dtype_ctypes" ^
-    src/visionTuner.py
+REM pyinstaller --onefile --name="Vision Tuner" --noconfirm ^
+REM     --distpath=output/dist --workpath=output/build --specpath=output/spec ^
+REM     --hidden-import="numpy.core._dtype_ctypes" ^
+REM     src/visionTuner.py
 rmdir /s /q "src/__pycache__/"
 xcopy /y "src\visionTuner.ui" "output\dist"
-xcopy /y /s "src\img\*" "dist\img"
+xcopy /y /s "src\img\*" "output\dist\img\"
